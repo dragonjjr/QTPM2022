0a4b28e507efb2faf69f4618e807be055f6f3eef
nakhuong269 <75614193+nakhuong269@users.noreply.github.com>
2022-10-19 15:23:30 +0700

    fix

--- Working/03_Source/01_Source Code/models/db.js
+++ Working/03_Source/01_Source Code/models/db.js
@@ -1,5 +1,16 @@
-const { Sequelize } = require('sequelize');
-module.exports = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/testcinema')
+const { Sequelize } = require("sequelize");
+module.exports = new Sequelize(
+  process.env.DATABASE_URL ||
+    "postgres://postgres:postgres@localhost:5432/testcinema",
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
 // const { Sequelize } = require('sequelize');
 // module.exports = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/testcinema', {
